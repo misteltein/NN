08483b4dfd0bf33daaba5abb2da2d04a664bd227
misteltein <itogahibiki@gmail.com>
2020-12-21 18:39:40 +0900

    updated

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ X_train = sc.transform(data_train[:, :2])
 X_test = sc.transform(data_test[:, :2])
 
 params = {
-    'hidden_layer_sizes': [(50,50,50,50,50,50,50,50,50,50,50)],
+    'hidden_layer_sizes': [(10,10,10,10,10,10,10,10,10,10,10)],
     'activation': ['relu'],
     'solver': ['adam'],
     'alpha': [0.0000001],
@@ -24,7 +24,11 @@ params = {
 
 #model = MLPClassifier(max_iter=1000)
 #gscv = GridSearchCV(model,params,cv=1,verbose=3,scoring='accuracy',n_jobs=-1)
-gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50),alpha=0.0000001,verbose=True)
+#gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10),alpha=0.0000001,verbose=True)
+#gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(100,100,100,100,100,100,100,100,100,100),alpha=0.0000001,verbose=True)#0.99312
+#gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(200,200,200,200,200,200,200,200,200,200),alpha=0.0000001,verbose=True,learning_rate='adaptive')#0.98404
+#gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50),alpha=0.0000001,verbose=True,learning_rate='adaptive')#0.98716
+gscv = MLPClassifier(max_iter=1000,hidden_layer_sizes=(100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100),alpha=0.0000001,verbose=True,learning_rate='adaptive')#0.98716
 gscv.fit( X_train, y_train )
 
 print(gscv.score(X_train, y_train))
