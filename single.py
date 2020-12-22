0f825825e7d2d207df4af51971e37fe7b4f6d3f7
misteltein <itogahibiki@gmail.com>
2020-12-22 21:23:46 +0900

    pep8

--- single.py
+++ single.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 
-data = np.loadtxt( 'spiral3.csv', delimiter = ',' )
+data = np.loadtxt('spiral3.csv', delimiter=',')
 data_train, data_test = train_test_split(data, test_size=0.25, random_state=0)
 y_train = data_train[:, 2].astype(int)
 y_test = data_test[:, 2].astype(int)
@@ -12,9 +12,13 @@ sc = preprocessing.StandardScaler().fit(data_train[:, :2])
 X_train = sc.transform(data_train[:, :2])
 X_test = sc.transform(data_test[:, :2])
 
-clf = MLPClassifier(verbose=True,hidden_layer_sizes=(100),alpha=1.0e-8,max_iter=1000)
+clf = MLPClassifier(
+    verbose=True,
+    hidden_layer_sizes=(100),
+    alpha=1.0e-8,
+    max_iter=1000)
 
-clf.fit( X_train, y_train )
+clf.fit(X_train, y_train)
 y_hat = clf.predict(X_test)
 
 print(clf.score(X_train, y_train))
@@ -22,6 +26,5 @@ print(clf.score(X_test, y_test))
 
 fig = plt.figure()
 ax = fig.add_subplot()
-plt.scatter(X_test[:,0],X_test[:,1],c=y_hat,s=1,alpha=0.5)
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_hat, s=1, alpha=0.5)
 plt.show()
-
